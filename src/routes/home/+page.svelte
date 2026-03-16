e5a572907b082016587bb80fb5aed043d95c090f
PierreElmen <pierre.elmen@gmail.com>
2026-03-16 23:34:23 +0100

    feat: add modern home component with enhanced features and UI improvements

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import HomeComponent from '../../components/view/home/HomeComponent.svelte';
+	// import HomeComponentModern from '../../components/view/home/HomeComponentModern.svelte';
 </script>
 
 <HomeComponent />
